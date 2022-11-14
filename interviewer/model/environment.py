84a433d8eb41cb63a18439a5b095afe8c0af7b93
piroyoung <piroyoung@users.noreply.github.com>
2022-11-14 22:20:43 +0900

    add: assertion

--- interviewer/model/environment.py
+++ interviewer/model/environment.py
@@ -6,4 +6,5 @@ class Environments:
     @property
     def teams_incoming_webhook(self) -> str:
         tiw: str = os.environ.get("TEAMS_INCOMING_WEBHOOK")
-        return "" if tiw is None else tiw
+        assert tiw
+        return tiw
